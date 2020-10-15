3d6950cebcfd28fc998f2ebecdab5d78c3804adc
Lộc Phạm <phamxuanloc2000@gmail.com>
2020-10-15 18:17:52 +0700

    khung project

---
+++ projectit3180/src/main/java/com/example/projectit3180/Projectit3180Application.java
@@ -0,0 +1,13 @@
+package com.example.projectit3180;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectit3180Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projectit3180Application.class, args);
+    }
+
+}
